8547794bdede38f08696ce998e88c252317809bb
thiswallz <thiswallz@gmail.com>
2013-02-13 12:34:29 -0200

    Update app/view/Main.js

--- app/view/Main.js
+++ app/view/Main.js
@@ -7,31 +7,21 @@ Ext.define('Ejemplo4Nested.view.Main', {
     ],
     config: {
         tabBarPosition: 'bottom',
-
         items: [
             {
                 title: 'Welcome',
                 iconCls: 'home',
-
                 styleHtmlContent: true,
                 scrollable: true,
-
                 items: {
                     docked: 'top',
                     xtype: 'titlebar',
                     title: 'Welcome to Sencha Touch 2'
-                },
-
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
+                }
             },
             {
                 title: 'Lista',
                 iconCls: 'action',
-
                 items: [
                     {xtype: 'listaregiones'}
                 ]
